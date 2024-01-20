c1820f579dfdf84380ef8eecafdeee183c8820a3
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-21 01:46:40 +0200

    changed name of table to users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --
 -- Creates new table
-CREATE TABLE [IF NOT EXISTS] user (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
