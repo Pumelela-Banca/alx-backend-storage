9597e940876b619ff88cd325a7f2e4da51f89a57
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-21 01:56:21 +0200

    added comma

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
 --
 -- Creates new table
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
