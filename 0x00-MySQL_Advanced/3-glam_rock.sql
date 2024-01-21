270db9e28fd6dd0ebd83eaffb07ad1a9555d7311
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-21 02:01:45 +0200

    lists bands by style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,6 @@
 --
--- Creates new table
+-- band style by life span
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC
